ddd0ed501a126ae57f1702883b6ad8fb61b90285
yuebing <123456>
2018-04-08 17:21:29 +0800

    排序算法-冒泡加快速

---
+++ src/main/java/com/ybin/arithmetic/Sort.java
@@ -0,0 +1,101 @@
+package com.ybin.arithmetic;
+
+/**
+ * @author yuebing
+ * @version 1.0 2018/4/8
+ * @Description 排序算法
+ */
+public class Sort {
+
+    /**
+     * 冒泡排序
+     *
+     * @param a
+     */
+    public static void buddingSort(int[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        int tmp;
+        for (int i =0; i < a.length-1; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if (a[i] <= a[j]) {
+                    continue;
+                }
+                tmp = a[j];
+                a[j] = a[i];
+                a[i] = tmp;
+            }
+        }
+    }
+
+    /**
+     * 鸡尾酒排序,冒泡排序的改进版
+     *
+     * @param a
+     */
+    public static void cocktailSort(int[] a) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        int left = 0 ,right = a.length -1, tmp;
+        for (; ;) {
+            if (left >= right)
+                break;
+            for (int i = left; i < right; i++) {
+                if (a[i] > a[i+1]) {
+                    tmp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = tmp;
+                }
+            }
+            right--;
+            for (int i = right; i > left; i--) {
+                if (a[i] < a[i-1]) {
+                    tmp = a[i];
+                    a[i] = a[i-1];
+                    a[i-1] = tmp;
+                }
+            }
+            left++;
+        }
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param a
+     * @param low
+     * @param hight
+     */
+    public static void quickSort(int[] a, int low, int hight) {
+        if (a == null || a.length == 0) {
+            return;
+        }
+        if (low >= hight) {
+            return;
+        }
+        int left = low, right = hight, key = a[low], tmp;
+        //第一次循环的时候找出数组在基准点左右侧的数据
+        while (left < right) {
+            while (left < right && a[right] >= key) {
+                right--;
+            }
+            while (left < right && a[left] <= key) {
+                left++;
+            }
+            if (left < right) {
+                tmp = a[left];
+                a[left] = a[right];
+                a[right] = tmp;
+            }
+        }
+        //将基准点换到数组left与right相同的位置
+        a[low] = a[left];
+        a[left] = key;
+        //递归排序基准点左侧的数据
+        quickSort(a, low, right - 1);
+        //递归排序基准点右侧的数据
+        quickSort(a, right + 1 , hight);
+    }
+}
---
+++ src/test/java/arithmetic/SortTest.java
@@ -0,0 +1,39 @@
+package arithmetic;
+
+import com.ybin.arithmetic.Sort;
+import org.junit.Test;
+
+/**
+ * @author yuebing
+ * @version 1.0 2018/4/8
+ * @Description
+ */
+public class SortTest {
+
+    @Test
+    public void buddingSortTest() {
+        int[] arr = new int[]{15, -1, 22, 10, 0, 17, 13};
+        Sort.buddingSort(arr);
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+
+    @Test
+    public void cocktailSortTest() {
+        int[] arr = new int[]{15, -1, 22, 10, 0, 17, 13};
+        Sort.cocktailSort(arr);
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+
+    @Test
+    public void quickSortTest() {
+        int[] arr = new int[]{15, -1, 22, 10, 0, 17, 13};
+        Sort.quickSort(arr, 0, arr.length-1);
+        for (int a : arr) {
+            System.out.println(a);
+        }
+    }
+}
